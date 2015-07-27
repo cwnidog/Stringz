8da0f9df28a89008587c5384b13033bfb1e7d1f5
cwnidog <cwnidog@comcast.net>
2015-07-27 16:28:58 -0700

    First commit.

---
+++ Stringz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stringz
+//
+//  Created by John Leonard on 7/27/15.
+//  Copyright (c) 2015 John Leonard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
